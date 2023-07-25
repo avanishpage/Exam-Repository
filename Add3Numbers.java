d0568f92667aea0671ac635b2f39838840aa8ef9
avanish page <avanishpp@gmail.com>
2023-07-25 08:12:12 +0530

    first commit with add 3 numbers

---
+++ Add3Numbers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Add3Numbers {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("enter 3 numbers : ");
+		int no1=sc.nextInt();
+		int no2=sc.nextInt();
+		int no3=sc.nextInt();
+		
+		System.out.println("the sum of 3 numbers is : "+(no1+no2+no3));
+		
+
+	}
+
+}
